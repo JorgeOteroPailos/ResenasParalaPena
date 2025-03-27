cee981a5efc72d6713ff49846b048ef83c1fe4eb
AlonsoBelosoL <alonsobeloso@gmail.com>
2025-03-27 16:26:20 +0100

    Create validaciones.js

---
+++ reseñasparalapeña/js/validaciones.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+    if (form) {
+      form.addEventListener('submit', function (e) {
+        const password = document.getElementById('reg_password');
+        const password2 = document.getElementById('reg_password2');
+        if (password && password2 && password.value !== password2.value) {
+          e.preventDefault(); // Evita el envío del formulario
+          alert('Las contraseñas no coinciden');
+        }
+      });
+    }
+  });
